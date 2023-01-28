f49c149b868b37885e874d1f03558a57ddd1aa09
hoomanjb <hoomanjavan74@gmail.com>
2023-01-28 19:07:57 +0330

    add users model

--- app/models.py
+++ app/models.py
@@ -16,3 +16,15 @@ class Post(Base):
         self.title = title
         self.content = content
         self.published = published
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
